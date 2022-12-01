dd290beba6fa9e52d9637ce5bfd0d7d08e397f70
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-01 18:59:16 +0100

    Update

--- Assets/Scripts/VRRig.cs
+++ Assets/Scripts/VRRig.cs
@@ -3,37 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class VRMap
+public class MapTransform
 {
     public Transform vrTarget;
-    public Transform rigTarget;
-    public Vector3 positionOffset;
-    public Vector3 rotationOffset;
+    public Transform IKTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
 
-    public void Map()
+    public void MapVRAvatar()
     {
-        rigTarget.position = vrTarget.TransformPoint(positionOffset);
-        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(rotationOffset);
+        IKTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        IKTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
     }
 }
 
 public class VRRig : MonoBehaviour
 {
-    public VRMap head;
-    public VRMap leftHand;
-    public VRMap rightHand;
+    [SerializeField] private MapTransform head;
+    [SerializeField] private MapTransform leftHand;
+    [SerializeField] private MapTransform rightHand;
 
-    public Transform headConstraint;
-    private Vector3 headBodyOffset;
+    [SerializeField] private float turnSmoothness;
 
-    private void Start()
-    {
-        headBodyOffset = transform.position - headConstraint.position;
-    }
+    [SerializeField] private Transform IKHead;
+
+    [SerializeField] private Vector3 headBodyOffset;
 
     private void LateUpdate()
     {
-        transform.position = headConstraint.position + headBodyOffset;
-        transform.forward = Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized;
+        transform.position = IKHead.position + headBodyOffset;
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(IKHead.forward, Vector3.up).normalized, Time.deltaTime * turnSmoothness); ;
+        head.MapVRAvatar();
+        leftHand.MapVRAvatar();
+        rightHand.MapVRAvatar();
     }
 }
